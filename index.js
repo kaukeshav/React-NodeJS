af087f061d7795bb148c69074004717c468f9ab6
Keshav Kaushik <keshavkau@gmail.com>
2019-08-12 13:30:26 +0530

    Messaged changed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = express(); // single app - creates app 
+const express = require("express");
+const app = express(); // single app - creates app
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'not there' });
+app.get("/", (req, res) => {
+  res.send({ hi: "Akshay Lr." });
 });
 
 const PORT = process.env.PORT || 5000;
